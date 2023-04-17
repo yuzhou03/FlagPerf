af1ec8005e8da3e1df73652d9d671cf0ef13bf8b
zhouyu <zhouyu@baai.ac.cn>
2023-04-17 14:10:57 +0800

    update nv configs

--- training/nvidia/mask_rcnn-pytorch/config/config_A100x1x8.py
+++ training/nvidia/mask_rcnn-pytorch/config/config_A100x1x8.py
@@ -1,7 +1,7 @@
 vendor:str = "nvidia"
 data_dir: str = "/mnt/data/maskrcnn/train/"
-train_batch_size = 16
-eval_batch_size = 16
+train_batch_size = 8
+eval_batch_size = 8
 
 
 dist_backend = "nccl"
@@ -11,7 +11,7 @@ adam_beta2 = 0.999
 adam_eps = 1e-08
 gradient_accumulation_steps = 1
 warmup = 0.1
-lr = 0.01
+lr = 0.02
 log_freq = 1
 seed = 10483
 max_samples_termination = 5553080
