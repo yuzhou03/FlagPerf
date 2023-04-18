a6e316f32f5e20f5fe8051803e5590739dd40310
zhouyu <zhouyu@baai.ac.cn>
2023-04-18 18:15:52 +0800

    resolve conflicts

--- training/nvidia/mask_rcnn-pytorch/config/config_A100x1x4.py
+++ training/nvidia/mask_rcnn-pytorch/config/config_A100x1x4.py
@@ -1,12 +1,7 @@
 vendor: str = "nvidia"
 data_dir: str = "/mnt/data/maskrcnn/train/"
-<<<<<<< HEAD
 train_batch_size = 16
 eval_batch_size = 16
-=======
-train_batch_size = 8
-eval_batch_size = 8
->>>>>>> 913d4a1d978ef456b6d5a68ab094e0fe2bdac454
 
 dist_backend = "nccl"
 weight_decay = 1e-4
@@ -15,15 +10,11 @@ adam_beta2 = 0.999
 adam_eps = 1e-08
 gradient_accumulation_steps = 1
 warmup = 0.1
-<<<<<<< HEAD
 lr = 0.01
-=======
-lr = 0.02
->>>>>>> 913d4a1d978ef456b6d5a68ab094e0fe2bdac454
 log_freq = 1
 seed = 10483
 max_samples_termination = 5553080
 training_event = None
 
-# resume: str = "output/checkpoint/model_13.pth"
-# start_epoch: int = 13
+resume: str = "output/checkpoint/model_0.pth"
+start_epoch: int = 0
